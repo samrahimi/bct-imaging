22714a4ea041bde1b1b6fda9c5165a7aa9db3d37
Sam Rahimi <sam@Sams-MacBook-Pro.local>
2018-10-22 11:49:34 -0400

    experimental tread dispatcher and global cache

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const request= require("request")
-const imaging = require("./lib/imaging.js")
-
+import request from "request";
+import { drawChart } from "./lib/imaging.js";
+const uuid = require('uuid')
 const DATA_URL = 'http://localhost:5000/api/prices?xyz123';
 
-const IMG_REFRESH_INTERVAL = 30*60*1000; //testing
+const IMG_REFRESH_INTERVAL = 30*60*1000; //production
+const RENDER_THROTTLE_MS = 1000;         //save your server
 const IMG_WIDTH = 138
 const IMG_HEIGHT = 26
 
 const GLOBAL_CACHE = {}
+function newFunction() {
+    var id;
+}
 
 function getLatestData() {
     try{ 
-    request(DATA_URL, { json: true }, (err, res, body) => {
+    request(DATA_URL, { json: true }, (err, _res, body) => {
         if (err) {  
             return console.log(err); 
         } else {
@@ -26,23 +30,44 @@ function getLatestData() {
     }
 }
 
-function render(name, timeseries, fieldName) {
+function render(name, timeseries, _fieldName) {
     let t= timeseries.slice(0, 98);
     var sparklineData = t.map(x => x.close)
-    imaging.drawChart(sparklineData, name.toLowerCase(), 600, 600)
+    drawChart(sparklineData, name.toLowerCase(), 600, 600)
 }
 
-function dispatchRenderingThread(dataSeries) {
-    setTimeout(function(dataRow) {
-            try {
-                render(dataRow.coin, dataRow.priceHistory.Data, "close");
-            } 
-            catch(ex)
-            {
-                console.log(ex.toString())
-            }        
-        }, 0, dataSeries)
+async function dispatchRenderingThread(dataSeries) {
+    return thread(
+
+    )
+
+    return new Promise((resolve, reject) => {
+        let startDate= Date.now();
+        try {
+            render(dataSeries.coin, dataSeries.priceHistory.Data, "close");
+            let endDate = Date.now();
+            resolve({
+                    thread_id: threadId,
+                    status: "done", 
+                    coin: dataSeries.coin,
+                    render_time_ms:(endDate - startDate)
+            })
+        } 
+        catch(ex)
+        {
+            console.log(ex.toString())
+            reject({
+                thread_id: threadId,
+                status: "error", 
+                coin: dataSeries.coin || "?",
+                render_time_ms:(endDate - startDate)
+            });
+        }        
+
+    })
 }
+
+
 
 function generateImages(historicalPrices) {
     for (var i=0; i<historicalPrices.length; i++) {
@@ -51,11 +76,9 @@ function generateImages(historicalPrices) {
 }
 
 
-module.exports={
-    startRenderingJob: function() {
-        getLatestData();
-        renderInterval=setInterval(getLatestData, IMG_REFRESH_INTERVAL)
-    }
+export function startRenderingJob() {
+    getLatestData();
+    renderInterval = setInterval(getLatestData, IMG_REFRESH_INTERVAL);
 }
 
 
